0eb929d204cc3917acfdc0958ba088cbc1727bd1
Asem <ismailovaasem363@gmail.com>
2025-03-26 20:32:22 +0500

    hw18 done

---
+++ hw18.js
@@ -0,0 +1,26 @@
+const result = document.querySelector("h2");
+const lowLim = document.querySelector("#lower");
+const upLim = document.querySelector("#upper");
+const genBtn = document.querySelector("#genBtn");
+const clearBtn = document.querySelector("#clearBtn");
+
+genBtn.addEventListener("click", function (min, max) {
+    event.preventDefault();
+
+    min = lowLim.value;
+    max = upLim.value;
+
+    if(min < max) {
+        let randomNum = Number(Math.floor(Math.random() * (max - min + 1))) + Number(min);
+        result.innerHTML = randomNum;
+        console.log(randomNum);
+    } else {
+        result.innerHTML = "Invalid Input";
+    }
+});
+
+clearBtn.addEventListener("click", function () {
+    lowLim.value = '';
+    upLim.value = '';
+    result.innerHTML = ' ';
+});
